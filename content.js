7b227530074b3c5c9d98c709b997e6b297f65239
Chlor <24903342+chlorophylls@users.noreply.github.com>
2018-11-18 19:23:30 -0500

    Implemented search functions
    Groundwork for search/filtering functions laid, corrected CSS to make the overlay sticky. Still need to work on regex and examining how the DOM structure of DW works

--- content.js
+++ content.js
@@ -1,13 +1,16 @@
 // content.js; Runs in the foreground of the HTML page.
 
-document.addEventListener("DOMContentLoaded", evt => {
-	// When document is ready, create overlay div.
-	createOverlay();
+// List to edit/test.
+var list = ["fire emblem awakening", "mcu", "dceu", "toriko", "m/m", "ota"];
 
-	// List to edit/test.
-	var list = ["Saab", "Volvo", "BMW", "Apocrypha", "Zero", "Hollow", "Ataraxia", "Last", "Encore", "Heaven's", "Feel"];
-	updateOverlay(list); // Updates the overlay with the current list of terms.
-});
+//document.addEventListener("DOMContentLoaded", evt => {
+// When document is ready, create overlay div.
+createOverlay();
+
+updateOverlay(list); // Updates the overlay with the current list of terms.
+
+filter(); // Filters the 
+//});
 
 // Sets the CSS styling properties of the overlay div.
 function createOverlay() {
@@ -25,17 +28,17 @@ function createOverlay() {
 
 	/** Set CSS styling for the elements. **/
 	// CSS styling for wrapper.
-	overlayWrapper.style.top = "5%";
-	overlayWrapper.style.position = "absolute";
+	overlayWrapper.style.top = "10px";
 	overlayWrapper.style.left = "50%";
+	overlayWrapper.style.position = "fixed";
 	overlayWrapper.style.zIndex = "999";
-	overlayWrapper.style.minWidth = "60%";
+	overlayWrapper.style.minWidth = "70%";
 
 	// CSS styling for the title div (CURRENTLY FILTERING TO).
 	overlayTitleDiv.innerText = "CURRENTLY FILTERING TO:"; // Inner text.
 	overlayTitleDiv.style.color = "#ffffff";
 	overlayTitleDiv.style.padding = "0.4em";
-	overlayTitleDiv.style.fontSize = "1.25em";
+	overlayTitleDiv.style.fontSize = "1em";
 	overlayTitleDiv.style.backgroundColor = "#78C2AD";
 
 	// CSS styling for actual overlay itself.
@@ -56,10 +59,11 @@ function updateOverlay(list) {
 	for (i = 0; i < list.length; i++) {
 		var term = document.createElement("div");
 		term.innerText = list[i];
-		
+
 		// CSS styling for each overlay-term div.
 		term.className = "overlay-term";
 		term.style.backgroundColor = "#78C2AD";
+		term.style.fontSize = "0.75em";
 		term.style.padding = "0.5em";
 		term.style.margin = "0.5em";
 		term.style.color = "#ffffff";
@@ -68,5 +72,33 @@ function updateOverlay(list) {
 		//term.style.border = "1px solid #3c8671";
 
 		document.getElementById("filter-overlay-content").appendChild(term);
+	}
+}
+
+// Goes through all the Dreamwidth comment threads (which typically have the class "comment-depth-1").
+function filter() {
+	var commentThreads = document.getElementsByClassName("comment-depth-1"); // list of HTML elements that are DW comment threads.
+
+	// For each comment thread, go to the topmost one (which should be the original comment anyway).
+	for (i = 0; i < commentThreads.length; i++) {
+		var toplevel = commentThreads[i].querySelector('.comment'); // gets the toplevel comment itself
+		var commentTitle = toplevel.querySelector('.comment-title > span').innerHTML; // searches for the comment title/header and gets the inner HTML (inside of the span tags that are in headers).
+		checkList(commentTitle);
+	}
+}
+
+// Checks if the input text contains any phrase in the list. Returns true if it does.
+function checkList(text) {
+	length = list.length;
+
+	var regexList = new RegExp(list.join("|"), "i"); // case insensitive matching
+
+	while (length--) { // loops through entire list of whitelisted terms and check if the input text ("text") matches any of them.
+		if (regexList.test(text)) {
+			console.log(text); // returns the input text if it does contain a whitelisted term.
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
