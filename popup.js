1b95649f617cdcb4fa81c909ad9486a15447693d
chlorophylls <24903342+chlorophylls@users.noreply.github.com>
2018-11-20 16:21:30 -0500

    Added toggle button functionality (somewhat)

--- popup.js
+++ popup.js
@@ -1,9 +1,13 @@
 // The main array/list that stores the data for what terms to filter to.
 var list = ["Fate", "Stay", "Night", "Unlimited", "blade", "works", "Strange", "Fake", "Prototype", "Apocrypha", "Zero", "Hollow", "Ataraxia", "Extra", "Last", "Encore", "Heaven's", "Feel", "Carnival", "Phantasm"];
 
+
 // Document ready function. 
 document.addEventListener("DOMContentLoaded", evt => {
-
+	
+	// Gets the current state of the extension (whether it's turned off or on).
+	// var currentState = document.getElementById("extension-toggle").className;	
+	
 	// Binds the Enter key such that if it's pressed in the input box, it's treated as a button click/submission.
 	document.getElementById("list-add-input").addEventListener("keydown", function (e) {
 		if (e.keyCode === 13) { //checks whether the pressed key is "Enter"
@@ -13,29 +17,26 @@ document.addEventListener("DOMContentLoaded", evt => {
 
 	// TESTING FOR ARRAY/LIST FUNCTIONS
 	updateList(list);
-	
-	var toggle = document.getElementById("extension-toggle");
-	
-	
+
 	document.getElementById("list-add-button").onclick = addTerms;
 
 	// Binds the deleteSelected function to any button with the id 'delete-selected-button'.
 	document.getElementById("delete-selected-button").onclick = deleteSelected;
 
 	/** options.html ONLY FUNCTIONS **/
-	
+
 	// Binds the deleteAll function to any button with the id 'delete-all-button'.
 	document.getElementById("delete-all-button").onclick = deleteAll;
-	
+
 	// Binds the importFromFile function to any button with the id 'import-button'.
 	document.getElementById("import-button").onclick = importFromFile;
-	
+
 	// Binds the exportPlaintext function to any button with the id 'export-button'.
 	document.getElementById("export-button").onclick = exportPlaintext();
-	
+
 	// Binds the exportPlaintext function to any button with the id 'export-button'.
 	document.getElementById("export-plain-copy-button").onclick = copyToClipboard;
-	
+
 	// Get the modal
 	var modal = document.getElementById('export-modal');
 
@@ -46,31 +47,31 @@ document.addEventListener("DOMContentLoaded", evt => {
 	var span = document.getElementsByClassName("modal-close")[0];
 
 	// When the user clicks on the button, open the modal 
-	btn.onclick = function() {
-    	modal.style.display = "block";
+	btn.onclick = function () {
+		modal.style.display = "block";
 	};
 
 	// When the user clicks on <span> (x), close the modal
-	span.onclick = function() {
-    	modal.style.display = "none";
+	span.onclick = function () {
+		modal.style.display = "none";
 	};
 
 	// When the user clicks anywhere outside of the modal, close it
-	window.onclick = function(event) {
+	window.onclick = function (event) {
 		if (event.target == modal) {
 			modal.style.display = "none";
 		}
 	};
-	
+
 });
 
 // Populates filter-list divs based on an array/list.
 function updateList(list) {
 	var currentList = document.getElementById("filter-list");
-	
+
 	// Reloads/clears the list for the updated one.
 	currentList.innerHTML = "";
-	
+
 	// Goes through the list and repopulates it with the updated data.
 	// For each item in the list array, create a div and append it to the filter-list div.
 	for (i = 0; i < list.length; i++) {
@@ -91,7 +92,7 @@ function addTerms() {
 	var inputList = input.trim().split(/\s*,\s*/);
 
 	for (i = 0; i < inputList.length; i++) {
-		list.push(inputList[i]);		
+		list.push(inputList[i]);
 	}
 	// Clears the input field.
 	inputField.value = '';
@@ -109,8 +110,8 @@ function deleteTerm(button) {
 // In options.html. The delete-all-button deletes all terms saved in the database.
 function deleteAll() {
 	if (confirm('Are you sure you want to delete all of your saved filter terms?')) {
-		list.length = 0; 	// Clears the entire list.
-		updateList(list); 	// Updates the list divs.
+		list.length = 0; // Clears the entire list.
+		updateList(list); // Updates the list divs.
 	} else {
 		return; // Do nothing!
 	}
@@ -120,17 +121,17 @@ function deleteAll() {
 function deleteSelected() {
 	var checkboxes = document.querySelectorAll("input[class=list-check]:checked");
 	var indexesToRemove = []; // Indexes of the selected terms to delete.
-	
+
 	for (var i = 0; i < checkboxes.length; i++) {
 		if (checkboxes[i].checked) {
 			var deleteTerm = checkboxes[i].closest("#filter-list > div.filter-list-row").getAttribute("data-index"); // Takes the data-index of the row selected for deletion.
 			indexesToRemove.push(deleteTerm); // And pushes the index into a temporary list that stores which items at these indexes need to be cleaned out.
 		}
 	}
-	
+
 	// Goes through the main list and removes the elements in the indexes that are stored in indexesToRemove.
 	// Removes items from list in reverse order so it avoids messing up the indexes of the yet-to-be-removed items.
-	for (var j = indexesToRemove.length - 1; j >= 0; j--){
+	for (var j = indexesToRemove.length - 1; j >= 0; j--) {
 		list.splice(indexesToRemove[j], 1);
 	}
 	updateList(list); // Updates the list divs because rows were removed from the main list.
@@ -143,21 +144,30 @@ function importFromFile() {
 
 // In options.html. When the user clicks the export button (export-button), a modal shows up (export-modal) and provides a textarea that they can copy from for their list in plaintext form.
 // This function will fill in the textarea.
-function exportPlaintext(){
+function exportPlaintext() {
 	var textarea = document.getElementById("export-plain-textarea");
 	textarea.value = list.toString();
 }
 
-function copyToClipboard(){
-	
-var textarea = document.getElementById("export-plain-textarea");
-	textarea.select();
-  /* Copy the text inside the text field */
-  document.execCommand("copy");
+function copyToClipboard() {
 
+	var textarea = document.getElementById("export-plain-textarea");
+	textarea.select();
+	
+	/* Copy the text inside the text field */
+	document.execCommand("copy");
 }
 
 // Toggles whether the button/extension is off or on.
-function toggleState(){
-	
+function switchState(button) {
+	if (button.className == "dw-filter-on") { // If already on, turn off.
+		button.className = "dw-filter-off";
+		console.log("switch is off");
+	} else { // Else, it's already off, so turn on.
+		button.className = "dw-filter-on";
+		console.log("switch is on");
+		//chrome.storage.sync.set({ "extension-state": "dw-filter-on" });
+		//var status = chrome.storage.sync.get(["extension-state"]);
+		//console.log(status);
+	}
 }
