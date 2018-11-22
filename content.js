14ddf4c7f0ecd4f2c942b7e8120a2aebf1b3759f
Chlor <24903342+chlorophylls@users.noreply.github.com>
2018-11-22 17:40:35 -0500

    Created content.css to handle the styling for the divs created in content.js

--- content.js
+++ content.js
@@ -25,31 +25,10 @@ function createOverlay() {
 	overlayWrapper.id = "filter-overlay-wrapper";
 	overlay.id = "filter-overlay-content";
 
+	overlayTitleDiv.innerText = "CURRENTLY FILTERING TO:"; // Inner text for the title div.
+	
 	overlay.appendChild(overlayTitleDiv); // Attaches the title div to the actual overlay div.
 	overlayWrapper.appendChild(overlay); // Attaches the actual overlay div (the content) to the wrapper.
-
-	/** Set CSS styling for the elements. **/
-	// CSS styling for wrapper.
-	overlayWrapper.style.top = "10px";
-	overlayWrapper.style.left = "50%";
-	overlayWrapper.style.position = "fixed";
-	overlayWrapper.style.zIndex = "999";
-	overlayWrapper.style.minWidth = "70%";
-
-	// CSS styling for the title div (CURRENTLY FILTERING TO).
-	overlayTitleDiv.innerText = "CURRENTLY FILTERING TO:"; // Inner text.
-	overlayTitleDiv.style.color = "#ffffff";
-	overlayTitleDiv.style.padding = "0.4em";
-	overlayTitleDiv.style.fontSize = "1em";
-	overlayTitleDiv.style.backgroundColor = "#78C2AD";
-
-	// CSS styling for actual overlay itself.
-	overlay.style.position = "relative";
-	overlay.style.left = "-50%";
-	overlay.style.textAlign = "center";
-	//overlay.style.minHeight = "12em";
-	overlay.style.borderRadius = "1em";
-	overlay.style.backgroundColor = "rgba(211, 211, 211, 0.5)"; // This is just the same gray (#D3D3D3) that's used in popup, just with transparency.
 
 	// Appends the overlay divs to the HTML page's body.
 	document.body.appendChild(overlayWrapper);
@@ -65,20 +44,12 @@ function updateOverlay(list) {
 
 		// CSS styling for each overlay-term div.
 		term.className = "overlay-term";
-		term.style.backgroundColor = "#78C2AD";
-		term.style.fontSize = "0.75em";
-		term.style.padding = "0.5em";
-		term.style.margin = "0.5em";
-		term.style.color = "#ffffff";
-		term.style.display = "inline-block";
-		term.style.borderRadius = "0.5em";
-		//term.style.border = "1px solid #3c8671";
 
-		document.getElementById("filter-overlay-content").appendChild(term);
+		document.getElementById("filter-overlay-content").appendChild(term); // Appends the term to the content div.
 	}
 }
 
-// Goes through all the Dreamwidth comment threads (which typically have the class "comment-thread").
+// Goes through all the Dreamwidth comments (which typically have the class "comment-thread").
 function filter() {
 	console.log("Filtering..."); // Debugging
 
